b5fa14535a37fd4531754640c91df2e3127449a6
santabeard <emmamarti@live.se>
2024-02-27 17:01:53 +0100

    create initial database
    gfhdhedfheb

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sqlite3.h>
 
 int main(void) {
-	printf("Hello, world!\n");
+	sqlite3 *db;
+
+	if(sqlite3_open("data.db", &db)) {
+		printf("Cannot open database\n");
+		exit(EXIT_FAILURE);
+	} else {
+		printf("Data base was opened successfully\n");
+	}
+
+	sqlite3_close(db);
+
 }
